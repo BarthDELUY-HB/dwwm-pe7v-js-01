288c4045c3818ec8b8a12084e436e227475d8867
Barth DELUY <barth@barthdeluy.me>
2023-08-08 15:22:40 +0200

    Verif du chiffre

--- regexp/main.js
+++ regexp/main.js
@@ -4,10 +4,7 @@ const MIN_PASSWORD_LENGTH = 8;
 
 function checkLength(event)
 {
-    console.log(event);
     const pSize = document.querySelector('.size');
-    console.log("event.value : " + event.target.value.length);
-    console.log("elt.value : " + document.querySelector('#password').value.length);
     // Sur les events key, la valeur du champ est changée *après* l'event
     if (event.target.value.length >= MIN_PASSWORD_LENGTH) {
         pSize.classList.replace('error', 'ok');
@@ -18,7 +15,13 @@ function checkLength(event)
 }
 function checkContainsDigit(event)
 {
-
+    const regex = /\d/;
+    const pNum = document.querySelector('.num');
+    if (regex.test(event.target.value)) {
+        pNum.classList.replace('error', 'ok');
+        return true;
+    }
+    pNum.classList.replace('ok', 'error');
     return false;
 }
 function checkContainsUppercase(event)
